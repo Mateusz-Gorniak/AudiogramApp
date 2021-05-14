2c67e0e23e593e5604dc44b758b675d409d26401
Mateusz <mateusz.gorniak@student.wat.edu.pl>
2021-05-14 20:49:20 +0200

    Starting Layout

---
+++ app/src/main/java/pl/gorniak/audiogramapp/CalibrationActivity.java
@@ -0,0 +1,14 @@
+package pl.gorniak.audiogramapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalibrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calibration);
+    }
+}
